683beecb36bcc1c6cc9daafefd826ec3c1cf3b15
pooh23896 <noah.rekers@gmail.com>
2026-02-12 12:58:44 +0100

    zaklamp item, room light

--- Assets/Scripts/Outline.cs
+++ Assets/Scripts/Outline.cs
@@ -1,68 +1,73 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
-[RequireComponent(typeof(Renderer))]
 public class Highlight : MonoBehaviour
 {
-    private Renderer rend;
-    private Material[] originalMaterials;
-    private Material[] glowMaterialsInstance;
-    private bool highlightActive = false;
-
-    [Header("Glow Outline Settings")]
-    public Material outlineGlowMaterial;
+    [Header("Glow Settings")]
     public Color glowColor = Color.white;
-    [Range(0.01f, 1f)]
-    public float maxAlpha = 0.5f;
-    [Range(0f, 5f)]
     public float glowIntensity = 2f;
-    [Range(1f, 20f)]
     public float fadeSpeed = 10f;
 
-    [Header("World Text Settings")]
-    public bool showItemText = true;      // Toon text bij highlight
-    public float textY = 2f;              // Vaste hoogte
-    public float textScale = 0.25f;
-
+    private bool highlightActive = false;
     private float currentAlpha = 0f;
+
+    private Renderer[] renderers;
+    private Dictionary<Renderer, Material[]> originalMaterials = new();
+    private Dictionary<Renderer, Material[]> glowMaterials = new();
+
     private TextMeshPro worldText;
+    public float textY = 2f;
+    public float textScale = 0.25f;
+    public bool showItemText = true;
     private Transform cam;
 
     void Start()
     {
         cam = Camera.main.transform;
 
-        rend = GetComponent<Renderer>();
-        originalMaterials = rend.materials;
-
-        glowMaterialsInstance = new Material[originalMaterials.Length];
-        for (int i = 0; i < glowMaterialsInstance.Length; i++)
+        renderers = GetComponentsInChildren<Renderer>();
+        foreach (var rend in renderers)
         {
-            glowMaterialsInstance[i] = new Material(outlineGlowMaterial);
-            SetGlowProperties(glowMaterialsInstance[i], 0.01f);
+            originalMaterials[rend] = rend.materials;
+            Material[] glowMats = new Material[rend.materials.Length];
+
+            for (int i = 0; i < glowMats.Length; i++)
+            {
+                // Alleen emissive copy maken
+                Material mat = new Material(rend.materials[i]);
+                if (mat.HasProperty("_EmissiveColor"))
+                {
+                    mat.SetColor("_EmissiveColor", glowColor * 0.01f);
+                    mat.EnableKeyword("_EMISSIVE_COLOR");
+                }
+                glowMats[i] = mat;
+            }
+
+            glowMaterials[rend] = glowMats;
         }
 
-        CreateWorldText(); // Altijd aanmaken, ongeacht showItemText
+        CreateWorldText();
     }
 
     void Update()
     {
-        float targetAlpha = highlightActive ? maxAlpha : 0.01f;
+        float targetAlpha = highlightActive ? 1f : 0.01f;
         currentAlpha = Mathf.MoveTowards(currentAlpha, targetAlpha, fadeSpeed * Time.deltaTime);
 
-        for (int i = 0; i < glowMaterialsInstance.Length; i++)
-            SetGlowProperties(glowMaterialsInstance[i], currentAlpha);
+        foreach (var rend in renderers)
+        {
+            Material[] mats = glowMaterials[rend];
+            for (int i = 0; i < mats.Length; i++)
+            {
+                if (mats[i].HasProperty("_EmissiveColor"))
+                    mats[i].SetColor("_EmissiveColor", glowColor * glowIntensity * currentAlpha);
+            }
 
-        if (currentAlpha > 0.02f)
-        {
-            Material[] combined = new Material[originalMaterials.Length + glowMaterialsInstance.Length];
-            originalMaterials.CopyTo(combined, 0);
-            glowMaterialsInstance.CopyTo(combined, originalMaterials.Length);
-            rend.materials = combined;
-        }
-        else
-        {
-            rend.materials = originalMaterials;
+            if (currentAlpha > 0.02f)
+                rend.materials = mats;
+            else
+                rend.materials = originalMaterials[rend];
         }
 
         UpdateWorldText();
@@ -77,52 +82,18 @@ public class Highlight : MonoBehaviour
         worldText.alignment = TextAlignmentOptions.Center;
         worldText.fontSize = 5;
         worldText.color = Color.white;
-
-        // Altijd uit in begin
         worldText.gameObject.SetActive(false);
 
-        // Zet prefab naam of object naam
-        PrefabReferenceAuto pr = GetComponent<PrefabReferenceAuto>();
-        if (pr != null && pr.prefab != null)
-            worldText.text = pr.prefab.name;
-        else
-            worldText.text = gameObject.name.Replace("(Clone)", "");
+        worldText.text = gameObject.name.Replace("(Clone)", "");
     }
 
     void UpdateWorldText()
     {
         if (worldText == null) return;
-
-        // Activeer alleen als highlight aan én showItemText true
         worldText.gameObject.SetActive(highlightActive && showItemText);
-
-        // Positioneer boven object
-        Vector3 pos = new Vector3(transform.position.x, textY, transform.position.z);
-        worldText.transform.position = pos;
+        worldText.transform.position = new Vector3(transform.position.x, textY, transform.position.z);
         worldText.transform.localScale = Vector3.one * textScale;
-
-        // Draai altijd naar camera
         worldText.transform.rotation = Quaternion.LookRotation(worldText.transform.position - cam.position);
-    }
-
-    private void SetGlowProperties(Material mat, float alpha)
-    {
-        Color col = new Color(glowColor.r, glowColor.g, glowColor.b, alpha);
-        mat.SetColor("_BaseColor", col);
-
-        if (mat.HasProperty("_EmissiveColor"))
-        {
-            mat.SetColor("_EmissiveColor", glowColor * glowIntensity);
-            mat.EnableKeyword("_EMISSIVE_COLOR");
-        }
-
-        if (mat.HasProperty("_Surface"))
-            mat.SetFloat("_Surface", 1f);
-
-        if (mat.HasProperty("_ZWrite"))
-            mat.SetFloat("_ZWrite", 0);
-
-        mat.renderQueue = 3100;
     }
 
     public void EnableHighlight() => highlightActive = true;
